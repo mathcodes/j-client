d2ae29d871fbe289b698c7f83852f34a83ab947a
mathcodes <jonpchristie@gmail.com>
2023-01-17 12:41:07 -0500

    build

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,7 @@
 import Image from 'next/image'
-import jcircle from '@/images/logos/jcircle.png'
-import { Button } from '@/components/Button'
-import { Container } from '@/components/Container'
-import logoLaravel from '@/images/logos/laravel.svg'
-import logoMirage from '@/images/logos/mirage.svg'
-import logoStatamic from '@/images/logos/statamic.svg'
-import logoStaticKit from '@/images/logos/statickit.svg'
-import logoTransistor from '@/images/logos/transistor.svg'
-import logoTuple from '@/images/logos/tuple.svg'
+import jcircle from '../images/gallery/jCircle.png'
+import { Container } from './Container'
+
 import {
   TwitterIcon,
   InstagramIcon,
